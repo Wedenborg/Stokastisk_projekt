7dda9edb64da7e973da6129a13ea3a5e0aec563c
HannahEinfeldt <hannah@einfeldt.net>
2018-06-11 15:52:09 +0200

    Startet på task 7

--- Part2.R
+++ Part2.R
@@ -5,3 +5,18 @@ Q = matrix(
   nrow = 5,
   ncol = 5)
 Q <- t(Q)
+
+
+## Task 7
+
+woman = rep(1,1000) # Create 1000 women
+N.iter = 1000
+
+count = matrix(0, ncol = 5, nrow = N.iter) #
+
+for (i in 1:N.iter){
+  while (woman[i]<5){
+    count[i,woman[i]] = count[i,woman[i]]+1
+    woman[i]=sample(x = c(1:5), size =1, replace =TRUE, prob = P[woman[i],])
+  }
+}
