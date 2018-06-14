32e936a90cf7648a6a8f77e1ef44751310453ba2
Wedenborg <emiliewedenborg@gmail.com>
2018-06-14 11:58:05 +0200

    13

--- Part3.R
+++ Part3.R
@@ -51,12 +51,13 @@ sum(Y[,26])
 
 ############ Opgave 13
 
-woman = rep(1,1000) # Create 1000 women
-N.iter = 1000
+woman = rep(1,10) # Create 1000 women
+N.iter = 10
+TT = seq(48,1200, 48)
 
 count = matrix(0, ncol = 5, nrow = N.iter) #
-
-for (i in 1:N.iter){
+i = 1
+while (i<=10){
   while (woman[i]<5){
     event = rexp(1,rate=-Q[woman[i],woman[i]])
     count[i,woman[i]] = event
@@ -67,9 +68,53 @@ for (i in 1:N.iter){
     else{
       woman[i]=5
     }
-    if (woman[i]!=Y[i,t]){
-      woman[i]=1
+  }
+  Y_test = vector()
+  Y_test[1] = 1
+  for (t in TT){
+    if (t < count[i,1]){
+      Y_test[t/48+1] = 1
+    } else if (t< (count[i,1]+count[i,2])){
+      Y_test[t/48+1] = 2
+    } else if (t< (count[i,1]+count[i,2]+count[i,3])){
+      Y_test[t/48+1] = 3
+    } else if (t< (count[i,1]+count[i,2]+count[i,3]+count[i,4])){
+      Y_test[t/48+1] = 4
+    } else {
+      Y_test[t/48+1] = 5
     }
+  }
+  if (all(Y_test == Y[i,])){
+    i = i+1
+    print(i)
+  } else {
+    woman[i] = 1
   }
 
 }
+
+
+Q0 = matrix(c(-0.005,0,0,0,0,
+            0.00125,-0.005,0,0,0,
+            0.00125,0.002,-0.005,0,0,
+            0.00125,0.0015,0.0025,-0.005,0,
+            0.00125,0.0015,0.0025,0.005,0),5,5)
+rowSums(Q0)
+
+S = vector()
+for (i in 1:5){
+  count_test = count
+  for (j in 1:5){
+    if (i < j ){
+    N[i,j] = sum(count_test[i] > 0 && count_test[j] > 0 )
+    # If a person has jumped from i to j they are not count until new i ´.
+    count_test[count_test[i] > 0 && count_test[j] > 0] = 0
+    }
+  }
+  S[i] = sum(coun[,i])
+  }
+Q1[i,j] = N[i,j]/S[i]
+
+
+#####################
+## Task 13 2.0
