6a941bee82d986d12d5ffe7bd5f09db0e7ef1691
jeppe johan waarkjær olsen <jeppe742@gmail.com>
2018-06-17 23:32:19 +0200

    Added some computer magic
    Code is now way faster, and a bit prettier

--- Part3.R
+++ Part3.R
@@ -1,6 +1,6 @@
 #################### Part 3
 Q = matrix(
-  c(-0.0085,  0.005,  0.0025,       0,   0.001,
+  c(-0.0085,  0.005,  0.0024,       0.0001,   0.001,
     0, -0.014,   0.005,   0.004,   0.005,
     0,      0,  -0.008,   0.003,   0.005,
     0,      0,       0,  -0.009,   0.009,
@@ -51,70 +51,115 @@ sum(Y[,26])
 
 ############ Opgave 13
 
-woman = rep(1,10) # Create 1000 women
-N.iter = 10
-TT = seq(48,1200, 48)
+sample.womans = function(Q, Y, N.women=1000, N.samples=100){
+  '
+  Samples some women in continous time, given a Q matrix
+  
+  Args:
+    Q : The probability matrix
+    Y : The state trajectory distribution to try and sample from
+    N.women : the number of women to sample
+    N.samples : the number of samples to try at each state change, in order to find a state satisfying the criterias
 
-count = matrix(0, ncol = 5, nrow = N.iter) #
-i = 1
-while (i<=10){
-  while (woman[i]<5){
-    event = rexp(1,rate=-Q[woman[i],woman[i]])
-    count[i,woman[i]] = event
-    if (woman[i] <4){
-      woman[i]=sample(x = c((woman[i]+1):5), size =1, replace =TRUE,
-                      prob =-(Q[woman[i],(woman[i]+1):5])/Q[woman[i],woman[i]])
-    }
-    else{
-      woman[i]=5
+  Returns:
+    S : the Sojurn time for each state (state 5 is be design always 0)
+    N : The number of women jumping from state i to j
+    states : the time spent in each state by each woman
+  '
+  woman = rep(1,N.women) # Create 1000 women
+  
+  states = matrix(0, ncol = 5, nrow = N.women) #
+  i = 1
+  
+  N = matrix(0, ncol=5, nrow = 5)
+  S = rep(0, 5)
+  
+  #Generate one woman at a time
+  for (i in 1:N.women){
+    
+    #Calculate the steps where the state is changed
+    state.changes = match(unique(Y[i,]), Y[i,])
+    
+    #Only loop over where the events where the woman changes states
+    t = 0
+    for(j in 2:length(state.changes)){
+        #Loop until we find a candidate
+        while(TRUE){
+        #Generate alot of event times
+        events = rexp(N.samples,rate=-Q[woman[i],woman[i]])
+        
+        #Generate alot of new states
+        if(woman[i]<4){
+          new.states = sample(x = c((woman[i]+1):5), size =N.samples, replace =TRUE,
+                           prob =-(Q[woman[i],(woman[i]+1):5])/Q[woman[i],woman[i]])
+        }
+        #if the woman is in state 4, the only new possible state is 5
+        else{
+          new.states = rep(5,100)
+        }
+        
+        #the simulated state should satisfy 
+        # 1. the new state being the same
+        # 2. the time of state change should be larger than the time of the step just before the timestep of Y
+        # 3. the time of state change should be less than or equal to the time of the first step of that state in Y
+        criteria = new.states ==  Y[i,state.changes[j]] & t+events>((state.changes[j]-1)*48-48) & t+events<=(state.changes[j]*48-48)
+        
+        #Check if we generated any candidates
+        if(any(criteria)){
+          #Just pick the first of the samples that satisfy the criteria
+          new.state = new.states[criteria][1]
+          event = events[criteria][1]
+          
+          #Update the value of N and S
+          N[woman[i], new.state] = N[woman[i], new.state] + 1
+          S[woman[i]] = S[woman[i]] + event
+          
+          #Update woman 
+          states[i, woman[i]] = event
+          woman[i] = new.state
+          t = t + event
+          
+          #Break the loop, and move on to next state change
+          break
+        }
+      }
     }
   }
-  Y_test = vector()
-  Y_test[1] = 1
-  for (t in TT){
-    if (t < count[i,1]){
-      Y_test[t/48+1] = 1
-    } else if (t< (count[i,1]+count[i,2])){
-      Y_test[t/48+1] = 2
-    } else if (t< (count[i,1]+count[i,2]+count[i,3])){
-      Y_test[t/48+1] = 3
-    } else if (t< (count[i,1]+count[i,2]+count[i,3]+count[i,4])){
-      Y_test[t/48+1] = 4
-    } else {
-      Y_test[t/48+1] = 5
-    }
-  }
-  if (all(Y_test == Y[i,])){
-    i = i+1
-    print(i)
-  } else {
-    woman[i] = 1
-  }
-
+  return(list("S" = S, "N" = N, "states" = states))
 }
 
-
-Q0 = matrix(c(-0.005,0,0,0,0,
+Qk = matrix(c(-0.005,0,0,0,0,
             0.00125,-0.005,0,0,0,
             0.00125,0.002,-0.005,0,0,
             0.00125,0.0015,0.0025,-0.005,0,
             0.00125,0.0015,0.0025,0.005,0),5,5)
-rowSums(Q0)
+rowSums(Qk)
 
-S = vector()
-for (i in 1:5){
-  count_test = count
-  for (j in 1:5){
-    if (i < j ){
-    N[i,j] = sum(count_test[i] > 0 && count_test[j] > 0 )
-    # If a person has jumped from i to j they are not count until new i ´.
-    count_test[count_test[i] > 0 && count_test[j] > 0] = 0
+converged = TRUE
+fejl = vector() 
+
+while(!converged){
+  
+  #Generate a sample
+  sample = sample.womans(Qk, Y)
+  
+  #create a copy of the current Q matrix, for comparison
+  Q_old = Qk
+  
+  #Update the new state
+  for(i in 1:5){
+    for(j in 1:5){
+      if(i<j){
+        Qk[i,j] = sample$N[i,j]/sample$S[i] 
+      }
     }
   }
-  S[i] = sum(coun[,i])
-  }
-Q1[i,j] = N[i,j]/S[i]
+  
+  #Add the error to the list of errors
+  fejl = c(fejl,max(abs(Qk-Q_old))) 
+  #Print the error, for our own sake
+  print(tail(fejl,1)) 
+  #Update congervence flag
+  converged = max(abs(Qk-Q_old))<10^(-3)
+}
 
-
-#####################
-## Task 13 2.0
