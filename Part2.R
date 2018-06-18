8033910e51da403a6a9ff514fd0a9c12a641f48d
HannahEinfeldt <hannah@einfeldt.net>
2018-06-18 11:01:31 +0200

    Rettet part 2 igennem of tilføjet lidt flere kommentarer

--- Part2.R
+++ Part2.R
@@ -1,5 +1,8 @@
+############# Part 2
 library(expm)
 rm(list=ls())
+
+#Initialize Q-matrix
 Q = matrix(
   c(-0.0085,  0.005,  0.0025,       0,   0.001,
           0, -0.014,   0.005,   0.004,   0.005,
@@ -11,15 +14,15 @@ Q = matrix(
 Q <- t(Q)
 
 
-## Task 7
-
+############# Task 7
+#Initializations
 woman = rep(1,1000) # Create 1000 women
 N.iter = 1000
-
 count = matrix(0, ncol = 5, nrow = N.iter) #
 
+# Simulate the lifetime of the women
 for (i in 1:N.iter){
-  while (woman[i]<5){
+  while (woman[i]<5){ #Woman has to have state under 5. otherwise they are dead 
     event = rexp(1,rate=-Q[woman[i],woman[i]])
     count[i,woman[i]] = event
     if (woman[i] <4){
@@ -29,14 +32,15 @@ for (i in 1:N.iter){
     else{
       woman[i]=5
     }
-
   }
 }
 
+# Visualize the lifetime for the women
 lifeTime = rowSums(count)
 hist(lifeTime)
 C = qchisq(p = c(0.025,0.975), df = 999)
-# Conf for stadard deviation: see notes part 2 file
+
+# Confidence interval for standard deviation
 conf=c(sqrt(999*var(lifeTime)/C[2]),sqrt(999*var(lifeTime)/C[1]))
 mean(lifeTime)
 
@@ -48,7 +52,7 @@ c(mean(lifeTime),Conf_control1,Conf_control2) #mean and CI for the mean
 #survival rate after 30.5 months
 (sum(rowSums(count[,1:2])>30.5 & (count[,3]!=0 | count[,4]!=0)))/1000
 
-############# Opgave 8
+############# Task 8
 Q = Q[1:4,1:4]
 funk = function(t){
   ones = rep(1,4)
@@ -74,19 +78,11 @@ Q2 = matrix(c(-0.00475,0,0,0,0
               ,0,0.002,0.003,-0.009,0
               ,0.001,0.005,0.005,0.009,0),5,5)
 
-woman = rep(1,1000) # Create 1000 women
-Q = matrix(
-  c(-0.0085,  0.005,  0.0025,       0,   0.001,
-    0, -0.014,   0.005,   0.004,   0.005,
-    0,      0,  -0.008,   0.003,   0.005,
-    0,      0,       0,  -0.009,   0.009,
-    0,      0,       0,       0,       0),
-  nrow = 5,
-  ncol = 5)
-N.iter = 1000
 
+woman = rep(1,1000) # Create 1000 women
 count_treat = matrix(0, ncol = 5, nrow = N.iter) #
 
+# Simulate the lifetime of the women with the treatment matrix
 for (i in 1:N.iter){
   while (woman[i]<5){
     event = rexp(1,rate=-Q2[woman[i],woman[i]])
@@ -98,18 +94,15 @@ for (i in 1:N.iter){
     else{
       woman[i]=5
     }
-
   }
 }
-
 cdf = function(lifeTime){
   bins = sort(lifeTime)
   cdf.value = cumsum(hist(lifeTime, breaks = bins, plot=FALSE)$count)/1000
   return(list("t"=tail(bins,-1), "cdf"=cdf.value))
 }
 
-# S function for non-treatment
-
+# Survival function for non-treatment
 life_cdf=cdf(lifeTime)
 
 S_func = function(cdf){
@@ -118,19 +111,18 @@ S_func = function(cdf){
 }
 plot(life_cdf$t,S_func(life_cdf),'l')
 
-# find s for treatment
+# Survival function for treatment
 lifeTimeTreat =rowSums(count_treat)
 lifeTreat_cdf=cdf(lifeTimeTreat)
 plot(lifeTreat_cdf$t,S_func(lifeTreat_cdf),'l')
 
-
-# plot with death rate for treat and non-treat
+# Plot with death rate for treat and non-treat
 plot(life_cdf$t,S_func(life_cdf),col = 'red',type='s',xlab = 't [months]', ylab = 'Women_Alive')
 lines(lifeTreat_cdf$t,S_func(lifeTreat_cdf),col='blue',type='s')
 title('Survival functions')
 
 
-#### Opgave 10
+############# Opgave 10
 # Log rank test
 N1 = head(S_func(life_cdf)*1000,-1)
 N2 = head(S_func(lifeTreat_cdf)*1000,-1)
@@ -143,20 +135,17 @@ O = abs(diff(N))
 N = tail(N,-1)
 N2 = tail(N2,-1)
 
+#Calculate the expected value
 E2 = O/N*N2
 
+#Calculate the variance
 V = (O*(N2/N)*(1-N2/N)*(N-O))/(N-1)
 
+#Calculate the test statistic
 Z = sum(O2-E2)/sum(V)
 
+#Find the p-value
 pValue = 1-pnorm(Z)
 
-
-####### Opgave 11
-# Man rykker stadie kontinuert og ikke l??ngere syg p?? en fast dag p?? m??neden
-# Man kan ikke ??ndre i sygdomsforl??bet fordi man allerede har determineret personens sygdomsforl??b
-### Dette kan g??res nemt i den anden ved at ??ndre Q
-# Erlang kan godt implementeres, men raterne skal laves om
-# Lav funktion, der implementerer Erlang. Tager Q-matrice
 
 
